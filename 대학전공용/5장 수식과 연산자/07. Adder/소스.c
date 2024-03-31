1dac938b4d33fe33cb8473ce066106fa1a010894
AWESOMEGUY5 <gusanal123@naver.com>
2024-03-31 23:39:09 +0900

    C언어 1~5장

--- 대학전공용/5장 수식과 연산자/07. Adder/소스.c
+++ 대학전공용/5장 수식과 연산자/07. Adder/소스.c
@@ -2,12 +2,13 @@
 
 int main(void)
 {
-	printf("복합 \"사칙\" 대입 연산자1\n");
-	int a= 10, b = 10, c = 33, d = 100, e = 25;
+	printf("<복합 대입 연산자1>\n");
+	int a= 10, b = 10, c = 33, d = 100, e = 25, f = 3, g = 8;
 
 	a += 1; b -= c; a *= a, d /= 25, e %= 5;
 
-	printf("a += %d \nb -= %d \nc *= %d \nd /= %d \ne= %d", a, b, c, d, e);
+	printf("a += 10: %d \nb -= c: %d \na *= a: %d \nd /= 25: %d \ne %%= 5: %d\n\n", a, b, c, d, e);
 	
-	printf("복합 ");
+	printf("<복합 대입 연산자2>\n");
+	printf("3 &= 1: %d \n8 |= 2: %d \n2 ^= 8: %d",f &= g, g |= f, f ^= g);
 }
